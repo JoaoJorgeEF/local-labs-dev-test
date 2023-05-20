3dcdc021a6c2a07c2970aff1bbf75a86384ef291
João Evangelista <joaojorgeevangelista@gmail.com>
2023-05-20 17:36:55 -0300

    new field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_193804) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_201142) do
   create_table "organizations", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "slug"
@@ -34,8 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_193804) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "organizations"
 end
